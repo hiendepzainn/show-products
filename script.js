a842accfc4c6fcd6e757c7cd9333dfcc09ddba85
Dinh Xuan Hien <124948520+hiendepzainn@users.noreply.github.com>
2025-02-08 21:49:05 +0700

    Search products by keyName

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Lấy thông tin về Category và detail sản phẩm để hiển thị ban đầu
+// Hiển thị All categories & All products
 var showCategorysAndAllProducts = () => {
   fetch("https://dummyjson.com/products")
     .then((result1) => {
@@ -63,6 +63,7 @@ var showCategorysAndAllProducts = () => {
     });
 };
 
+// Show products theo Category cụ thể
 var showProductsByCategory = (nameCategory) => {
   fetch("https://dummyjson.com/products")
     .then((result1) => {
@@ -103,11 +104,69 @@ var showProductsByCategory = (nameCategory) => {
       let rowShowProducts = document.querySelector(".show-products-block .row");
       rowShowProducts.innerHTML = htmlProductList;
     })
-    // Show products theo Category cụ thể
 
     .catch((error) => {
       console.log(error);
     });
 };
+
+//Show Products mà title có từ keyName
+var showProductsByKeyName = (keyName) => {
+  fetch("https://dummyjson.com/products")
+    .then((result1) => {
+      return result1.json();
+    })
+
+    .then((data) => {
+      console.log(data);
+      let products = data.products;
+      let htmlProductList = "";
+
+      products.forEach((element) => {
+        if (element.title.toLowerCase().includes(keyName)) {
+          htmlProductList += `<div class="outer-product col-sm-2 col-2">
+          <div class="product">
+            <div class="image">
+              <img
+                src=${element.thumbnail}
+                alt="image"
+              />
+            </div>
+
+            <div class="info-product">
+              <h4 class="name-product">${element.title}</h4>
+              <div class="details">
+                <span class="price">${element.price}</span>
+                <span>Còn lại: ${element.stock}sp</span>
+              </div>
+            </div>
+            <div class="discount">-${Math.round(
+              element.discountPercentage
+            )}%</div>
+          </div>
+        </div>`;
+        }
+      });
+
+      let rowShowProducts = document.querySelector(".show-products-block .row");
+      rowShowProducts.innerHTML = htmlProductList;
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+//Chức năng tìm kiếm
+var searchButton = document.querySelector("button");
+var searchInput = document.querySelector("#searchInput");
+console.log(searchButton);
+searchButton.addEventListener("click", () => {
+  if (searchInput.value == "") {
+    showCategorysAndAllProducts();
+  } else {
+    showProductsByKeyName(searchInput.value);
+  }
+});
 
 showCategorysAndAllProducts();
